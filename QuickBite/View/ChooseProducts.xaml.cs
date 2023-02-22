9e27ff3e5c6c11a9b3afaa5015303bfe0e2888fe
Andrey Kulikov <aa_kylukov@vk.com>
2023-02-22 19:30:45 +0300

    Fix

--- QuickBite/View/ChooseProducts.xaml.cs
+++ QuickBite/View/ChooseProducts.xaml.cs
@@ -5,26 +5,9 @@ using System.IO;
 
 public partial class ChooseProducts : ContentPage
 {
-    public string gett;
-    public int amt = 0;
-    public ObservableCollection<string> items =
-        new();
+    public ObservableCollection<string> items = new();
     readonly string[] products = new string[] { "Яйцо", "Хлеб", "Сливки", "Сыр", "Бекон", "Майонез", "Кутчуп", "Помидор", "Огурец", "Молоко", "Курица", "Говядина", "Свинина" };
-    readonly string[] links = new string[]
-        { "https://cdn-icons-png.flaticon.com/512/5222/5222963.png",
-          "https://cdn-icons-png.flaticon.com/512/3014/3014538.png",
-          "https://cdn-icons-png.flaticon.com/512/8008/8008185.png",
-          "https://cdn-icons-png.flaticon.com/512/517/517561.png",
-          "https://cdn-icons-png.flaticon.com/512/7471/7471954.png",
-          "https://cdn-icons-png.flaticon.com/512/4383/4383486.png",
-          "https://cdn-icons-png.flaticon.com/512/3387/3387851.png",
-          "https://cdn-icons-png.flaticon.com/512/1202/1202125.png",
-          "https://cdn-icons-png.flaticon.com/512/7830/7830699.png",
-          "https://cdn-icons-png.flaticon.com/512/6070/6070715.png",
-          "https://cdn-icons-png.flaticon.com/512/6679/6679109.png",
-          "https://cdn-icons-png.flaticon.com/512/1703/1703009.png",
-           "pig.png"
-        };
+    readonly string[] links = new string[] { "https://cdn-icons-png.flaticon.com/512/5222/5222963.png", "https://cdn-icons-png.flaticon.com/512/3014/3014538.png", "https://cdn-icons-png.flaticon.com/512/8008/8008185.png", "https://cdn-icons-png.flaticon.com/512/517/517561.png", "https://cdn-icons-png.flaticon.com/512/7471/7471954.png", "https://cdn-icons-png.flaticon.com/512/4383/4383486.png", "https://cdn-icons-png.flaticon.com/512/3387/3387851.png", "https://cdn-icons-png.flaticon.com/512/1202/1202125.png", "https://cdn-icons-png.flaticon.com/512/7830/7830699.png", "https://cdn-icons-png.flaticon.com/512/6070/6070715.png", "https://cdn-icons-png.flaticon.com/512/6679/6679109.png", "https://cdn-icons-png.flaticon.com/512/1703/1703009.png", "pig.png" };
     public ChooseProducts()
     {
         
@@ -40,7 +23,6 @@ public partial class ChooseProducts : ContentPage
 
     }
 
-
     private void AddToShoppingList(object sender, EventArgs e)
     {
         items.Add((sender as Button).Text);
@@ -49,7 +31,6 @@ public partial class ChooseProducts : ContentPage
 
     private async void GotoShoppingList(object sendet, EventArgs e)
     {
-        
         await Shell.Current.GoToAsync("ShoppingList");
     }
 }
