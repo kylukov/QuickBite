cf4e04348adaae439756a850eb20a7b6e679f061
Andrey Kulikov <aa_kylukov@vk.com>
2023-02-28 12:41:35 +0300

    Edit MVVM pattern

--- QuickBite/View/ChooseProducts.xaml.cs
+++ QuickBite/View/ChooseProducts.xaml.cs
@@ -6,21 +6,10 @@ using System.IO;
 public partial class ChooseProducts : ContentPage
 {
     public ObservableCollection<string> items = new();
-    readonly string[] products = new string[] { "Яйцо", "Хлеб", "Сливки", "Сыр", "Бекон", "Майонез", "Кутчуп", "Помидор", "Огурец", "Молоко", "Курица", "Говядина", "Свинина" };
-    readonly string[] links = new string[] { "https://cdn-icons-png.flaticon.com/512/5222/5222963.png", "https://cdn-icons-png.flaticon.com/512/3014/3014538.png", "https://cdn-icons-png.flaticon.com/512/8008/8008185.png", "https://cdn-icons-png.flaticon.com/512/517/517561.png", "https://cdn-icons-png.flaticon.com/512/7471/7471954.png", "https://cdn-icons-png.flaticon.com/512/4383/4383486.png", "https://cdn-icons-png.flaticon.com/512/3387/3387851.png", "https://cdn-icons-png.flaticon.com/512/1202/1202125.png", "https://cdn-icons-png.flaticon.com/512/7830/7830699.png", "https://cdn-icons-png.flaticon.com/512/6070/6070715.png", "https://cdn-icons-png.flaticon.com/512/6679/6679109.png", "https://cdn-icons-png.flaticon.com/512/1703/1703009.png", "pig.png" };
     public ChooseProducts()
     {
-        
         InitializeComponent();
-
         BindingContext = new ProductViewModel();
-
-        for (int i = 0; i < products.GetUpperBound(0) + 1; i++)
-        {
-            ((ProductViewModel)BindingContext).Products.Add(
-            new Model.Product(products[i], links[i]));
-        }
-
     }
 
     private void AddToShoppingList(object sender, EventArgs e)
--- QuickBite/ViewModel/ProductViewModel.cs
+++ QuickBite/ViewModel/ProductViewModel.cs
@@ -2,19 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuickBite.ViewModel
 {
-    internal class ProductViewModel
+    class ProductViewModel
     {
+        readonly string[] products = new string[] { "Яйцо", "Хлеб", "Сливки", "Сыр", "Бекон", "Майонез", "Кутчуп", "Помидор", "Огурец", "Молоко", "Курица", "Говядина", "Свинина" };
+        readonly string[] links = new string[] { "https://cdn-icons-png.flaticon.com/512/5222/5222963.png", "https://cdn-icons-png.flaticon.com/512/3014/3014538.png", "https://cdn-icons-png.flaticon.com/512/8008/8008185.png", "https://cdn-icons-png.flaticon.com/512/517/517561.png", "https://cdn-icons-png.flaticon.com/512/7471/7471954.png", "https://cdn-icons-png.flaticon.com/512/4383/4383486.png", "https://cdn-icons-png.flaticon.com/512/3387/3387851.png", "https://cdn-icons-png.flaticon.com/512/1202/1202125.png", "https://cdn-icons-png.flaticon.com/512/7830/7830699.png", "https://cdn-icons-png.flaticon.com/512/6070/6070715.png", "https://cdn-icons-png.flaticon.com/512/6679/6679109.png", "https://cdn-icons-png.flaticon.com/512/1703/1703009.png", "pig.png" };
+
         public ObservableCollection<Product> Products { get; set; }
 
         public ProductViewModel()
         {
             Products = new ObservableCollection<Product>();
+            for (int i = 0; i < products.GetUpperBound(0) + 1; i++)
+            {
+                Products.Add(new Model.Product(products[i], links[i]));
+            }
         }
     }
 }
