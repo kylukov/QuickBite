c6795125240f03f07707c9a257b620a14aa44826
Andrey Kulikov <aa_kylukov@vk.com>
2023-03-04 17:27:57 +0300

    Reverted

--- QuickBite/View/ChooseProducts.xaml.cs
+++ QuickBite/View/ChooseProducts.xaml.cs
@@ -20,5 +20,11 @@ public partial class ChooseProducts : ContentPage
         Console.WriteLine(sql_input);
     }
 
+    private void GotoShoppingList(object sendet, EventArgs e)
+    {
+        Console.WriteLine(sql_input.Substring(0, sql_input.Length - 1));
+
+        // Data.Database db = new Data.Database();
+        // Datab.find_receipt(sql_input.Substring(0, sql_input.Length - 1));
     }
 }
